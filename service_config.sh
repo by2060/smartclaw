2d27439e44585cc17b35a33056f42a64f8769132
wangbiao2 <wangbiao2@ultrapower.com.cn>
2026-06-23 12:16:59 +0800

    feat:新增tiktoken缓存文件，自动化脚本文件

--- service_config.sh
+++ service_config.sh
@@ -7,9 +7,9 @@ CONDA_ENV_NAME="smartclaw"
 # 是否启用 HTTPS
 ENABLE_HTTPS=true
 
-# 前端监听所有网卡，方便其他机器访问页面
+# 前端服务监听所有网卡
 FRONTEND_HOST="0.0.0.0"
-# 后端只监听本机，避免外部机器直接访问 API
+# 后端服务监听所有网卡
 BACKEND_HOST="0.0.0.0"
 # 前端服务代理到后端的目标地址
 PROXY_TARGET_HOST="127.0.0.1"
